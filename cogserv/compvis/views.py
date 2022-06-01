616c53487d7505bf170981b9c72d065859e45696
hafidzarifudin <58541210+hafidzarifudin@users.noreply.github.com>
2022-06-01 22:56:20 +0700

    Add files via upload

--- cogserv/compvis/views.py
+++ cogserv/compvis/views.py
@@ -1,48 +1,33 @@
 from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import redirect,render
+from ninja import NinjaAPI
 from PIL import Image
 from .forms import *
 from .ocr import *
-from .text_to_speech import *
-from .translator import *
 
-context = {
-    'result': '',
-    'available_languages': available_languages,
-    'source_language': 'en',
-    'target_language': 'id',
-    'selected_source': 'en',
-    'selected_target': 'id'
-}
+ocr_api = NinjaAPI(urls_namespace='ocr_api')
+temp_api = NinjaAPI(urls_namespace='temp_api')
 
 # Create your views here.
+@ocr_api.get('/')
 def index(request):
-    if request.method == 'GET':
-        return render(request, 'image_form.html', context)
+    form = OCRInputForm()
+    return render(request, 'image_form.html', {'form' : form})
 
+@ocr_api.post('/')
 def ocr_post(request):
-    if request.method == 'POST':
-        form = OCRInputForm(request.POST, request.FILES)
-        if form.is_valid():
-            ocr_result = sendOCR(Image.open(request.FILES['input_img']))
-            context['result'] = ocr_result
-        return render(request, 'image_form.html', context)
+    form = OCRInputForm(request.POST, request.FILES)
+    if form.is_valid():
+        ocr_result = sendOCR(Image.open(request.FILES['input_img']))
+        #Undefined OCR processing
 
-def translate_post(request):
-    if request.method == 'POST':
-        form = TranslatorInputForm(request.POST, request.FILES)
-        if form.is_valid():
-            translation_result = translate(request.POST['text'], request.POST['source_language'], request.POST['target_language'])
-            context['result'] = translation_result
-            context['selected_source'] = request.POST['source_language']
-            context['selected_target'] = request.POST['target_language']
-        return redirect(reverse('index'))
-
-def tts_post(request):
-    if request.method == 'POST':
-        form = TTSInputForm(request.POST, request.FILES)
-        if form.is_valid():
-            text_to_speech(request.POST['tts'])
-        return redirect(reverse('index'))
+        #Temporary render
+        return redirect('/success/')
+    else:
+        messages.error(request, 'Please submit a valid image file')
+        
+@temp_api.get('/')
+def success(request):
+    return HttpResponse('Nice')
+ 
